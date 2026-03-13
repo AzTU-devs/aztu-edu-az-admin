84b6dd8709c971b51816cc3ec64bb58b258f516f
Karam Shukurlu <146780222+shukurlukerem@users.noreply.github.com>
2026-03-13 18:24:31 +0400

    feat: menu

--- src/util/apiClient.ts
+++ src/util/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { RootState, store } from "../redux/store";
 
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+export const API_BASE_URL = 'https://api-aztu.karamshukurlu.site';
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
